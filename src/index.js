4c62945babcb1261a417af4d6a9b0e3cd71f1ef9
Avinash <avinashchandrabarik@gmail.com>
2024-05-06 11:20:38 +0530

    first phase

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: ["http://localhost:3000", "https://crazycart.netlify.app/"],
+    origin: ["http://localhost:3000", "https://crazycart.netlify.app"],
   })
 );
 
